8b722566bb723c82a51dac1962e2283acc504d14
Sandro Orsi <orsi.sandro.0@gmail.com>
2023-04-25 12:21:45 +0200

    begin work on multiscroll index

--- js/eindwerk.js
+++ js/eindwerk.js
@@ -1,12 +1,18 @@
 // Ece start
 let scrollPosition = window.pageYOffset;
-          window.onscroll = function() {
-            let currentScrollPos = window.pageYOffset;
-            if (scrollPosition > currentScrollPos) {
-              document.querySelector(".site-header").classList.add("visible");
-            } else {
-              document.querySelector(".site-header").classList.remove("visible");
-            }
-            scrollPosition = currentScrollPos;
-          }
+window.onscroll = function () {
+	let currentScrollPos = window.pageYOffset;
+	if (scrollPosition > currentScrollPos) {
+		document.querySelector(".site-header").classList.add("visible");
+	} else {
+		document.querySelector(".site-header").classList.remove("visible");
+	}
+	scrollPosition = currentScrollPos;
+};
 // Ece end
+
+$(document).ready(function () {
+	//sandro start
+	$("#multiscroll").multiscroll();
+	//sandro end
+});
