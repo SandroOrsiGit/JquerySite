e3deeb79fe67d1d9a444a771bf497ab20e152f7d
FUW <FUW@cvo.lan>
2023-04-26 16:02:24 +0200

    work on index

--- js/eindwerk.js
+++ js/eindwerk.js
@@ -13,6 +13,47 @@ window.onscroll = function () {
 
 $(document).ready(function () {
 	//sandro start
-	$("#multiscroll").multiscroll();
-	//sandro end
+	let buttonWidthArray = [];
+	let buttonHeightArray = [];
+	$indexButtons = $(".home-button");
+
+	$indexButtons.each(function () {
+		buttonWidthArray.push($(this).outerWidth());
+		buttonHeightArray.push($(this).outerHeight());
+	});
+
+	$("#multiscroll").multiscroll({});
+
+	$indexButtons.on("click", function () {
+		let index = $indexButtons.index(this);
+		switch (index) {
+			case 0:
+				window.location.href = "registration.html";
+				break;
+			case 1:
+				window.location.href = "curriculum.html";
+				break;
+		}
+	});
+
+	$indexButtons.hover(
+		function () {
+			let index = $indexButtons.index(this);
+			$(this)
+				.stop()
+				.animate({
+					height: buttonHeightArray[index] + 20,
+					width: buttonWidthArray[index] + 20,
+				});
+		},
+		function () {
+			let index = $indexButtons.index(this);
+			$(this).stop().animate({
+				height: buttonHeightArray[index],
+				width: buttonWidthArray[index],
+			});
+		}
+	);
 });
+
+//sandro end
