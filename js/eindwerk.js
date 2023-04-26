d5789bbc9301bdf4c55184a84a8a1650d100affd
Ece Dupont <ece.dupont@outlook.com>
2023-04-26 15:42:33 +0200

    jquery plugin added for header

--- js/eindwerk.js
+++ js/eindwerk.js
@@ -1,29 +1,10 @@
-// Ece start
-let scrollPosition = window.pageYOffset;
-window.onscroll = function () {
-	let currentScrollPos = window.pageYOffset;
-	if (scrollPosition > currentScrollPos) {
-		document.querySelector(".site-header").classList.add("visible");
-	} else {
-		document.querySelector(".site-header").classList.remove("visible");
-	}
-	scrollPosition = currentScrollPos;
-};
-
-// $(window).on("scroll", function () {
-// 	let scrollPosition = $(window).scrollTop();
-// 	let currentScrollPos = $(this).scrollTop();
-// 	if (scrollPosition > currentScrollPos) {
-// 	  $(".site-header").addClass("visible");
-// 	} else {
-// 	  $(".site-header").removeClass("visible");
-// 	}
-// 	scrollPosition = currentScrollPos;
-//   }); 
-
-// Ece end
-
 $(document).ready(function () {
+	// Ece start
+	$(".site-header").shyheader({
+		classname : "is-watching",
+	});
+	// Ece end
+	
 	//sandro start
 	$("#multiscroll").multiscroll();
 	//sandro end
