9f03e42f896c450e8aa10be1f63d4f5f24d6ae61
Sandro Orsi <orsi.sandro.0@gmail.com>
2023-04-27 12:15:39 +0200

    only load multiscrol on index

--- js/eindwerk.js
+++ js/eindwerk.js
@@ -16,59 +16,67 @@ $(document).ready(function () {
 	// FOR ACTIVE CLASS
 	let url = window.location.href;
 
-	$('nav ul li a').each(function() {
+	$("nav ul li a").each(function () {
 		if (this.href === url) {
-			$(this).closest('li').addClass('active');
+			$(this).closest("li").addClass("active");
 		}
 	});
 
-	$('nav ul li').click(function() {
-		$('nav ul li').removeClass('active');
-		$(this).addClass('active');
+	$("nav ul li").click(function () {
+		$("nav ul li").removeClass("active");
+		$(this).addClass("active");
 	});
 	// Ece end
 
 	//sandro start
-	let buttonWidthArray = [];
-	let buttonHeightArray = [];
-	$indexButtons = $(".home-button");
+	let pathlength = $(location).attr("href").length;
+	if (
+		$(location)
+			.attr("href")
+			.substring(pathlength - 10, pathlength) == "index.html"
+	) {
+		let buttonWidthArray = [];
+		let buttonHeightArray = [];
+		$indexButtons = $(".home-button");
 
-	$indexButtons.each(function () {
-		buttonWidthArray.push($(this).outerWidth());
-		buttonHeightArray.push($(this).outerHeight());
-	});
+		$indexButtons.each(function () {
+			buttonWidthArray.push($(this).outerWidth());
+			buttonHeightArray.push($(this).outerHeight());
+		});
 
-	$("#multiscroll").multiscroll({});
+		$("#multiscroll").multiscroll({});
 
-	$indexButtons.on("click", function () {
-		let index = $indexButtons.index(this);
-		switch (index) {
-			case 0:
-				window.location.href = "";
-				break;
-			case 1:
-				window.location.href = "curriculum.html";
-				break;
-		}
-	});
-
-	$indexButtons.hover(
-		function () {
+		$indexButtons.on("click", function () {
 			let index = $indexButtons.index(this);
-			$(this)
-				.stop()
-				.animate({
-					height: buttonHeightArray[index] + 20,
-					width: buttonWidthArray[index] + 20,
+			switch (index) {
+				case 0:
+					window.location.href = "";
+					break;
+				case 1:
+					window.location.href = "curriculum.html";
+					break;
+			}
+		});
+
+		$indexButtons.hover(
+			function () {
+				let index = $indexButtons.index(this);
+				$(this)
+					.stop()
+					.animate({
+						height: buttonHeightArray[index] + 20,
+						width: buttonWidthArray[index] + 20,
+					});
+			},
+			function () {
+				let index = $indexButtons.index(this);
+				$(this).stop().animate({
+					height: buttonHeightArray[index],
+					width: buttonWidthArray[index],
 				});
-		},
-		function () {
-			let index = $indexButtons.index(this);
-			$(this).stop().animate({
-				height: buttonHeightArray[index],
-				width: buttonWidthArray[index],
-			});
-		}
-	);
+			}
+		);
+	}
+
 	//sandro end
 });
